b4086c8edf2133dcbf8af8f4ab11afdd5a829188
Yash Gupta <128382912+devbeastyash@users.noreply.github.com>
2023-05-16 17:06:40 +0530

    Update CustomerWalletHistoryRepository.java

--- src/main/java/com/mutualfunds/mf/repository/CustomerWalletHistoryRepository.java
+++ src/main/java/com/mutualfunds/mf/repository/CustomerWalletHistoryRepository.java
@@ -12,11 +12,11 @@ import com.mutualfunds.mf.entity.CustomerWalletHistory;
 @Repository
 public interface CustomerWalletHistoryRepository extends JpaRepository<CustomerWalletHistory, Integer> {
 	
-	@Query(value = "SELECT * FROM CUSTOMER_WALLET_HISTORY WHERE CUSTOMER_ID=?", nativeQuery = true)
+	@Query(value = "SELECT * FROM customer_wallet_history WHERE CUSTOMER_ID=?", nativeQuery = true)
 	public List<CustomerWalletHistory> getHistories(int customerId);
 	
 	@Modifying
-	@Query(value = "INSERT INTO CUSTOMER_WALLET_HISTORY (CUSTOMER_ID, TRANSACTION_DATE, TRANSACTION_TYPE_ID, WALLET_AMOUNT, WALLET_ID) VALUES(?, CURRENT_TIMESTAMP, ?, ?, ?)", nativeQuery = true)
+	@Query(value = "INSERT INTO customer_wallet_history (CUSTOMER_ID, TRANSACTION_DATE, TRANSACTION_TYPE_ID, WALLET_AMOUNT, WALLET_ID) VALUES(?, CURRENT_TIMESTAMP, ?, ?, ?)", nativeQuery = true)
 	public void updateTransactionHistory(int customerId, int transactionTypeId, double walletAmount, int walletId);
 	
 }
