56f62c1d65d4c7c1c5b92422ff998b915e1403d0
devbeastyash <128382912+devbeastyash@users.noreply.github.com>
2023-05-16 14:54:46 +0530

    version_0.1

---
+++ src/main/java/com/mutualfunds/mf/MutualfundsApplication.java
@@ -0,0 +1,13 @@
+package com.mutualfunds.mf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualfundsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutualfundsApplication.class, args);
+	}
+
+}
